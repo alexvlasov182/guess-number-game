390b5aee5cad893897313adb41d58d4a7f7748ab
Oleksandr Vlasov <drumlife182@gmail.com>
2024-08-08 15:50:59 +0200

    Styling the Number Input element

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { TextInput, View, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet, ViewStyle } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen: React.FC = () => {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.numberInput} maxLength={2} />
       {/* BUTTONS */}
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -15,4 +15,28 @@ const StartGameScreen: React.FC = () => {
 
 export default StartGameScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  } as ViewStyle,
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
