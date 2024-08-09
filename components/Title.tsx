73462ff7cade5e20f9eb273e374fdc17c19650c8
Oleksandr Vlasov <drumlife182@gmail.com>
2024-08-09 09:57:19 +0200

    Created a Title component

---
+++ components/Title.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+interface TitleProps {
+  children: React.ReactNode; // Type for children prop
+}
+const Title: React.FC<TitleProps> = ({ children }) => {
+  return <Text style={styles.title}>{children}</Text>;
+};
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+    borderWidth: 2,
+    borderColor: "#fff",
+    padding: 12,
+  },
+});
--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import Title from "../components/Title";
+
 const GameScreen: React.FC = () => {
   return (
     <View style={styles.screen}>
-      <Text>Opponent's Guess</Text>
+      <Title>Opponent's Guess</Title>
       {/* GUESS */}
       <View>
         <Text>Higher or lower?</Text>
@@ -20,6 +22,6 @@ export default GameScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    padding: 12,
+    padding: 24,
   },
 });
